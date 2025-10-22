d860f99eb36af9335af9a38d7f31c5c1bd6fb7fa
Giorgi Banetishvili <g.banetishvili@gmail.com>
2025-10-22 16:23:21 +0400

    Privacy Content Page

--- src/app/info/privacy/privacy-content/privacy-content.component.ts
+++ src/app/info/privacy/privacy-content/privacy-content.component.ts
@@ -9,4 +9,10 @@ import { Component } from '@angular/core';
  * Component displaying the contents of the Privacy Statement
  */
 export class PrivacyContentComponent {
+  lang: string = 'en';
+
+  ngOnInit(): void {
+    this.lang = document.documentElement.lang;
+  }
+
 }
